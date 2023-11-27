2a6114fc1b90072e1648f2b0457ac516bc31fa8d
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-26 22:06:44 -0300

    Update ExerciseApplication.java

--- src/main/java/com/redis/exercise/ExerciseApplication.java
+++ src/main/java/com/redis/exercise/ExerciseApplication.java
@@ -4,13 +4,49 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.geo.Circle;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResult;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class ExerciseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExerciseApplication.class, args);
+	}
+
+
+	@Bean
+	ApplicationRunner geoGraph(ReactiveRedisTemplate <String,String> template){
+		return args -> {
+			var sicily = " Sicily";
+			var geoTemplate = template.opsForGeo();
+			var mapOfPoints = Map.of(
+					"messina",new Point(13.361389,38.11555556),
+					"catania",new Point(15.0876269,37.502669),
+					"Palermo",new Point(15.58333333,37.316667)
+			);
+			Flux.fromIterable(mapOfPoints.entrySet())
+					.flatMap(e -> geoTemplate.add(sicily,e.getValue(),e.getKey()))
+					.thenMany(geoTemplate.radius(sicily,new Circle(
+							new Point(15.583333,37.31667),
+							new Distance(10, RedisGeoCommands.DistanceUnit.KILOMETERS)
+
+					)))
+					.map(GeoResult::getContent)
+					.map(RedisGeoCommands.GeoLocation::getName)
+					.doOnNext(System.out::println)
+					.subscribe();
+
+
+
+		};
 	}
 
 	@Bean
